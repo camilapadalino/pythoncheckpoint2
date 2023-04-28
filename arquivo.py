00edab993005a1da33169b1373d6ed7704cfb803
anny <annydiascmd@gmail.com>
2023-04-28 18:54:10 -0300

    primo

--- arquivo.py
+++ arquivo.py
@@ -5,10 +5,45 @@ Anny Dias - 98295
 Camila Padalino - 98316
 Luana Cabezaolias - 99320
 '''
-cont = 1
-while cont <= 1000:  
-    n = int(input("Insira um número positivo: "))
 
-    if n == 0:
+def calcular_numero_primo(numero):
+    for i in range(2,numero):
+        if numero % i == 0:
+            return
+
+def primo_proximo(numero):
+    if calcular_numero_primo(numero):
+        return numero
+    
+    primo_maior = numero - 1
+    while not calcular_numero_primo(primo_maior):
+        primo_maior -= 1
+    menor_primo = numero + 1
+
+    while not calcular_numero_primo(menor_primo):
+        menor_primo += 1
+    if numero - primo_maior < menor_primo - numero:
+        return primo_maior
+    else:
+        return primo_maior
+    
+
+
+while True:
+    numero = int(input("Insira um número positivo: "))
+   
+    if numero == 0:
+        print("Fim do programa")
         break
-    elif n >= 1001 and n < 0:
+    elif numero < 0:
+        print("Número inválido")
+    elif numero > 1000:
+        print("Número inválido")
+    else: 
+        primo = primo_proximo(numero)
+        print(f"O número primo mais próximo de {numero} é {primo}")
+        
+
+    
+
+    
