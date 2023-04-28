ccde24bb33a7248f1588b8c7a229a470ccc51841
Luanacabezaolias <lu.cabezaolias@gmail.com>
2023-04-28 17:26:45 -0300

    entrada

--- arquivo.py
+++ arquivo.py
@@ -5,3 +5,9 @@ Anny Dias - 98295
 Camila Padalino - 98316
 Luana Cabezaolias - 99320
 '''
+cont = 1
+while cont <= 1000:  
+    n = int(input("Insira um número positivo: "))
+
+    if n <= 0:
+        break
