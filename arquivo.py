c262ab960cc0ae499295671f3c6e30bb9dcc993b
anny <annydiascmd@gmail.com>
2023-04-28 19:28:37 -0300

    primo

--- arquivo.py
+++ arquivo.py
@@ -1,37 +1,35 @@
 '''
 Checkpoint 2 Computational Thinking with Python
 Alunas:
-Anny Dias - 98295
-Camila Padalino - 98316
-Luana Cabezaolias - 99320
+Anny Dias - RM:98295
+Camila Padalino - RM:98316
+Luana Cabezaolias - RM:99320
 '''
 
+#Verificar se o número é primo
 def calcular_numero_primo(numero):
     for i in range(2,numero):
         if numero % i == 0:
-            return
+            return False
+    return True
 
+#Analisar o  número primo mais próximo
 def primo_proximo(numero):
-    if calcular_numero_primo(numero):
-        return numero
-    
-    primo_maior = numero - 1
-    while not calcular_numero_primo(primo_maior):
-        primo_maior -= 1
-    menor_primo = numero + 1
+    if numero < 2:
+        return 2
+    n = 0 
+    while True:
+        n += 1
+        if calcular_numero_primo(numero):
+            return numero
+        if calcular_numero_primo(numero-n):
+            return numero - n
+        if calcular_numero_primo(numero+n):
+            return numero + n
 
-    while not calcular_numero_primo(menor_primo):
-        menor_primo += 1
-    if numero - primo_maior < menor_primo - numero:
-        return primo_maior
-    else:
-        return primo_maior
-    
-
-
+#Pedindo o número ao usuário e verificando se é válido
 while True:
     numero = int(input("Insira um número positivo: "))
-   
     if numero == 0:
         print("Fim do programa")
         break
